b4cf9fbce1307ea132d5bfd2bb4de3a7257cbdf2
Chris Wolf <39116095+GRcwolf@users.noreply.github.com>
2020-06-17 20:25:00 +0200

    Implemented lightgallery in gallery.html

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// TODO: Change to es5 if IE support is necessary.
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('hi');
+    lightGallery(document.getElementById('lightgallery'));
+});
